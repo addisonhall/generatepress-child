6fced6a35add3a0dc127a54689edb22c98ee5098
Addison Hall <ah@addisonhalldesign.com>
2017-06-09 16:07:58 -0500

    Adding option to select menu theme location

--- inc/sub-menu-widget.php
+++ inc/sub-menu-widget.php
@@ -36,11 +36,12 @@ class gpc_sub_menu_widget extends WP_Widget {
   }
 
   // Creating widget front-end
-
   public function widget( $args, $instance ) {
+    $title = isset( $instance[ 'title' ] ) ? $instance[ 'title' ] : 'In this section';
+		$menu_theme_location = isset( $instance[ 'menu_theme_location' ] ) ? $instance[ 'menu_theme_location' ] : 'primary';
+
     require get_stylesheet_directory() . '/inc/sub-menu.php';
-    $title = apply_filters( 'widget_title', $instance['title'] );
-    $sub_menu_output = wp_nav_menu( array( 'theme_location' => 'primary', 'sub_menu' => true, 'link_after' => '', 'echo' => false ) );
+    $sub_menu_output = wp_nav_menu( array( 'theme_location' => $menu_theme_location, 'sub_menu' => true, 'echo' => false ) );
 
     if ( $sub_menu_output ) {
       // before and after widget arguments are defined by themes
@@ -56,25 +57,41 @@ class gpc_sub_menu_widget extends WP_Widget {
       
   // Widget Backend 
   public function form( $instance ) {
-    if ( isset( $instance[ 'title' ] ) ) {
-      $title = $instance[ 'title' ];
-    } else {
-      $title = __( 'In this section', 'gpc_sub_menu_widget_domain' );
-    }
+    $title = isset( $instance[ 'title' ] ) ? $instance[ 'title' ] : 'In this section';
+		$menu_theme_location = isset( $instance[ 'menu_theme_location' ] ) ? $instance[ 'menu_theme_location' ] : 'primary';
+    // Get menu theme locations
+    $menus = get_registered_nav_menus();
+
     // Widget admin form
     ?>
     <p>
       <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
       <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
     </p>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'menu_theme_location' ); ?>"><?php _e( 'Select menu theme location:' ); ?></label>
+      <select id="<?php echo $this->get_field_id( 'menu_theme_location' ); ?>" name="<?php echo $this->get_field_name( 'menu_theme_location' ); ?>">
+        <option value="0"><?php _e( '&mdash; Select &mdash;' ); ?></option>
+        <?php foreach ( $menus as $location => $description ) : ?>
+          <option value="<?php echo esc_attr( $location ); ?>" <?php selected( $menu_theme_location, $location ); ?>>
+            <?php echo esc_html( $description ); ?>
+          </option>
+        <?php endforeach; ?>
+      </select>
+    </p>
     <?php 
   }
     
   // Updating widget replacing old instances with new
   public function update( $new_instance, $old_instance ) {
-  $instance = array();
-  $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-  return $instance;
+    $instance = array();
+		if ( ! empty( $new_instance['title'] ) ) {
+			$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		}
+		if ( ! empty( $new_instance['menu_theme_location'] ) ) {
+			$instance['menu_theme_location'] = sanitize_text_field( $new_instance['menu_theme_location'] );
+		}
+		return $instance;
   }
 
 } // Class gpc_sub_menu_widget ends here
