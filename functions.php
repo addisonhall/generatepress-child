ffbbbe5cc6941f7af3c1ff143ffd5b3281d5de68
Addison Hall <ah@addisonhalldesign.com>
2017-06-12 17:55:26 -0500

    Cleaning up admin interface for Editor role

--- functions.php
+++ functions.php
@@ -12,6 +12,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 define( 'GPC_VERSION', '1.0');
 
 /**
+ * Get current user.
+ */
+$gpc_current_user = wp_get_current_user();
+
+/**
  * Hide admin bar.
  */
 show_admin_bar( false );
@@ -35,6 +40,17 @@ function gpc_scripts() {
 }
 
 /**
+ * Enqueue admin scripts and styles.
+ */
+add_action( 'admin_enqueue_scripts', 'gpc_load_admin_style' );
+function gpc_load_admin_style() {
+  global $gpc_current_user;
+  if ( in_array( 'editor', (array) $gpc_current_user->roles ) ) {
+    wp_enqueue_style( 'admin-editor-role-css', get_stylesheet_directory_uri() . '/admin/css/role-editor.css', false, GPC_VERSION, 'all' );
+  }
+}
+
+/**
  * Add .has-js class to html element.
  */
 add_action( 'generate_before_header','gpc_add_js_class' );  
@@ -52,6 +68,38 @@ function gpc_add_js_class() { ?>
 // add_filter( 'generate_metabox_capability', 'gpc_custom_metabox_capability', 10 );
 function gpc_gp_custom_metabox_capability() {
 	return 'view_gp_metaboxes';
+}
+
+/**
+ * Hide GeneratePress meta boxes on specified post types.
+ * Requires GeneratPress addons:
+ * @link https://generatepress.com/premium/
+ * @link https://generatepress.com/forums/topic/hide-screen-options-on-custom-post-type/#post-238774
+ */
+add_action( 'add_meta_boxes', 'gpc_remove_meta_boxes', 999 );
+function gpc_remove_meta_boxes() {
+  // Posts
+	remove_meta_box('generate_de_meta_box', 'post', 'side'); // Disable elements
+	remove_meta_box('generate_footer_widget_meta_box', 'post', 'side'); // Footer widgets
+	remove_meta_box('generate_layout_meta_box', 'post', 'side'); // Sidebar layout
+	remove_meta_box('generate_page_builder_meta_box', 'post', 'side'); // Page builder integration
+	remove_meta_box('generate_page_header_meta_box', 'post', 'normal'); // Page header
+  // Pages
+	remove_meta_box('generate_footer_widget_meta_box', 'page', 'side'); // Footer widgets
+	remove_meta_box('generate_page_builder_meta_box', 'page', 'side'); // Page builder integration
+}
+
+/**
+ * Give Editors access to menus and widgets.
+ */
+add_action( 'admin_menu', 'gpc_editor_menu_page_removing', 999 );
+function gpc_editor_menu_page_removing() {
+  global $gpc_current_user;
+  if ( in_array( 'editor', (array) $gpc_current_user->roles ) ) {
+    remove_menu_page( 'themes.php' );
+    add_menu_page( 'Menus', 'Menus', 'edit_theme_options', 'nav-menus.php', '', 'dashicons-menu', 60 );
+    add_menu_page( 'Widgets', 'Widgets', 'edit_theme_options', 'widgets.php', '', 'dashicons-layout', 61 );
+  }
 }
 
 /**
