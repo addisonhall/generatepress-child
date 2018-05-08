39dd34e889f973ce071c015b8964e02f047d91aa
Addison Hall <ah@addisonhalldesign.com>
2018-05-08 10:40:10 -0500

    Updating button-outline stuff

--- inc/styles.php
+++ inc/styles.php
@@ -13,66 +13,41 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-if ( !function_exists( 'gpc_base_inline_css' ) ) :
+if ( ! function_exists( 'gpc_base_inline_css_func' ) ) :
 
-  function gpc_base_inline_css() {
+  function gpc_base_inline_css_func() {
 
+    // Get our settings
     $gpc_settings = wp_parse_args( 
       get_option( 'generate_settings', array() ),
       generate_get_color_defaults()
     );
 
-    $gpc_visual_css = array (
-      
-      // Form button-outline
-      '.button.button-outline,
-      .button.button-outline:visited,
-      .woocommerce .button.button-outline,
-      .woocommerce .button.button-outline:visited' => array(
-        'border-color' => $gpc_settings['form_button_background_color'],
-        'color' => $gpc_settings['form_button_background_color']
-      ),
-      
-      // Form button-outline hover
-      '.button.button-outline:hover,
-      .button.button-outline:focus,
-      .woocommerce .button.button-outline:hover,
-      .woocommerce .button.button-outline:focus' => array(
-        'border-color' => $gpc_settings['form_button_background_color_hover'],
-        'color' => $gpc_settings['form_button_background_color_hover']
-      )
+    // Initiate our CSS class
+    $gpc_css = new GeneratePress_CSS;
 
-    );
+    // Form button-outline
+    $gpc_css->set_selector( '.button.button-outline, .button.button-outline:visited, .woocommerce .button.button-outline, .woocommerce .button.button-outline:visited' );
+		$gpc_css->add_property( 'border-color', esc_attr( $gpc_settings[ 'form_button_background_color' ] ) );
+		$gpc_css->add_property( 'color', esc_attr( $gpc_settings[ 'form_button_background_color' ] ) );
 
-    // Output the above CSS
-    $output = '';
-    foreach($gpc_visual_css as $k => $properties) {
-      if(!count($properties))
-        continue;
-      $temporary_output = $k . ' {';
-      $elements_added = 0;
-      foreach($properties as $p => $v) {
-        if(empty($v))
-          continue;
-        $elements_added++;
-        $temporary_output .= $p . ': ' . $v . '; ';
-      }
-      $temporary_output .= "}";
-      if($elements_added > 0)
-        $output .= $temporary_output;
-    }
-    
-    $output = str_replace(array("\r", "\n", "\t"), '', $output);
-    return $output;
+    // Form button-outline hover
+    $gpc_css->set_selector( '.button.button-outline:hover, .button.button-outline:focus, .woocommerce .button.button-outline:hover, .woocommerce .button.button-outline:focus' );
+		$gpc_css->add_property( 'border-color', esc_attr( $gpc_settings[ 'form_button_background_color_hover' ] ) );
+		$gpc_css->add_property( 'color', esc_attr( $gpc_settings[ 'form_button_background_color_hover' ] ) );
 
-  }
+    // Allow us to hook CSS into our output
+    // do_action( 'gpc_base_inline_css', $gpc_css );
 
-  /**
-   * Enqueue scripts and styles
-   */
-  add_action( 'wp_enqueue_scripts', 'gpc_inline_dynamic_scripts' );
-  function gpc_inline_dynamic_scripts() {
-    wp_add_inline_style( 'gpc-inline-dynamic', gpc_base_inline_css() );
+		// Return our dynamic CSS
+		return $gpc_css->css_output();
+
   }
 
 endif;
+
+add_action( 'wp_enqueue_scripts', 'gpc_add_inline_styles' );
+function gpc_add_inline_styles() {
+  $gpc_css = gpc_base_inline_css_func();
+  wp_add_inline_style( 'gpc-base', $gpc_css );
+}
