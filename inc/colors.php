bceb03c0ed9f7806c64203e34bff97c6e6a2577e
Addison Hall <ah@addisonhalldesign.com>
2020-07-29 10:11:01 -0500

    Theme colors from one array

--- inc/colors.php
+++ inc/colors.php
@@ -10,19 +10,42 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Insert custom colors
+ * ADD YOUR THEME COLORS HERE!
+ * Array of theme colors will be added to WordPress
+ */
+$gpc_theme_colors = array(
+    array(
+		'name'  => __( 'White', 'generatepress-child' ),
+		'slug'  => 'white',
+		'color'	=> '#ffffff',
+	),
+	array(
+		'name'  => __( 'Light gray', 'generatepress-child' ),
+		'slug'  => 'light-gray',
+		'color'	=> '#E3E6EB',
+	),
+	array(
+		'name'  => __( 'Black', 'generatepress-child' ),
+		'slug'  => 'black',
+		'color' => '#000000',
+    ),
+);
+
+/**
+ * Insert custom colors for the Customizer
  * @link https://docs.generatepress.com/article/generate_default_color_palettes/
  */
 add_filter( 'generate_default_color_palettes', 'gpc_custom_color_palettes' );
 function gpc_custom_color_palettes( $palettes ) {
-	$palettes = array(
-		'#000000',
-		'#FFFFFF',
-		'#efecea',
-		'#bed7d3',
-		'#4a7d6d',
-		'#d8751b',
-		'#fed850',
-	);
+    global $gpc_theme_colors;
+    $palettes = array();
+    for( $i = 0; $i < count( $gpc_theme_colors ); $i++ ) {
+        $palettes[] = $gpc_theme_colors[$i]['color'];
+    }
 	return $palettes;
 }
+
+/**
+ * Custom Gutenberg colors
+ */
+add_theme_support( 'editor-color-palette', $gpc_theme_colors );
