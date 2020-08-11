a6ad9cdffefec06f466de6171709a6f8d7e52bbf
Addison Hall <ah@addisonhalldesign.com>
2020-08-11 13:31:18 -0500

    Better GenerateBlocks button defaults from GP

--- inc/generateblocks.php
+++ inc/generateblocks.php
@@ -12,14 +12,31 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Set defaults for GenerateBlocks output
  * @link https://generatepress.com/forums/search/generateblocks+default/
+ * 
+ * Refer to these files for help with settings:
+ * wp-content/themes/generatepress/inc/css-output.php
+ * wp-content/plugins/generateblocks/includes/defaults.php
  */
 add_filter( 'generateblocks_defaults', function( $defaults ) {
 
     // get style settings from GeneratePress
     $gpc_theme_settings = wp_parse_args(
         get_option( 'generate_settings', array() ),
-        generate_get_color_defaults()
+        generate_get_color_defaults(),
+        generate_get_default_fonts()
     );
+
+    $og_defaults = generate_get_default_fonts( false );
+
+    $buttons_family = $og_defaults['font_buttons'];
+    if ( $og_defaults['font_buttons'] !== $gpc_theme_settings['font_buttons'] ) {
+        $buttons_family = $gpc_theme_settings['font_buttons'];
+    }
+
+    $buttons_font_size = $og_defaults['buttons_font_size'];
+    if ( '' !== $gpc_theme_settings['buttons_font_size'] ) {
+        $buttons_font_size = absint( $gpc_theme_settings['buttons_font_size'] );
+    }
 
     // BUTTONS
     $defaults['button']['backgroundColor'] = $gpc_theme_settings['form_button_background_color'];
@@ -30,8 +47,8 @@ add_filter( 'generateblocks_defaults', function( $defaults ) {
     $defaults['button']['paddingRight'] = '20';
     $defaults['button']['paddingBottom'] = '10';
     $defaults['button']['paddingLeft'] = '20';
-    $defaults['button']['fontFamily'] = generate_get_font_family_css( 'font_buttons', 'generate_settings', generate_get_default_fonts() );
-    $defaults['button']['fontSize'] = absint( $gpc_theme_settings['body_font_size'] );
+    $defaults['button']['fontFamily'] = $buttons_family;
+    $defaults['button']['fontSize'] = $buttons_font_size;
     $defaults['button']['fontSizeUnit'] = 'px';
     $defaults['button']['fontWeight'] = esc_attr( $gpc_theme_settings['buttons_font_weight'] );
 
