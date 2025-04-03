afba3a5d5293b2e5c00952e4344e2552bb8dd99c
Addison Hall <ah@addisonhalldesign.com>
2025-04-03 08:35:12 -0500

    scan scripts in wp_head as well to add nonce

--- inc/security.php
+++ inc/security.php
@@ -99,7 +99,7 @@ function gpc_add_custom_headers() {
     header( 'Strict-Transport-Security: max-age=31536000; includeSubDomains' );
     header( 'X-Frame-Options: SAMEORIGIN' );
     header( 'X-Content-Type-Options: nosniff' );
-    // header( 'Permissions-Policy: fullscreen=(self "https://addisonhall.design"), geolocation=*' ); // change the URL to match the site!
+    header( 'Permissions-Policy: fullscreen=(self "http://generatepress-child.test"), geolocation=*' ); // change the URL to match the site!
 }
 
 /**
@@ -129,19 +129,27 @@ function gpc_add_nonce_to_scripts( $html ) {
 }
 
 /**
- * Add nonces to footer scripts that are inserted "manually".
+ * Add nonces to head and footer scripts that are inserted "manually".
  * 
  * @see https://wordpress.stackexchange.com/questions/161971/overwrite-or-replace-code-in-wp-footer Provided the output buffer solution using wp_footer
  */
+add_action( 'wp_head', 'gpc_start_head_ob', 1 );
+function gpc_start_head_ob() {
+    ob_start( 'gpc_end_head_and_footer_ob_callback' );
+}
+add_action( 'wp_head', 'gpc_end_head_ob', 999 );
+function gpc_end_head_ob() {
+    ob_end_flush();
+}
 add_action( 'wp_footer', 'gpc_start_footer_ob', 1 );
 function gpc_start_footer_ob() {
-    ob_start( 'gpc_end_footer_ob_callback' );
+    ob_start( 'gpc_end_head_and_footer_ob_callback' );
 }
-add_action( 'wp_footer', 'gpc_end_footer_ob', 9999 );
+add_action( 'wp_footer', 'gpc_end_footer_ob', 999 );
 function gpc_end_footer_ob() {
     ob_end_flush();
 }
-function gpc_end_footer_ob_callback( $buffer ) {
+function gpc_end_head_and_footer_ob_callback( $buffer ) {
     global $csp_nonce;
     return gpc_add_nonce_to_dom_scripts( $buffer, $csp_nonce );
 }
