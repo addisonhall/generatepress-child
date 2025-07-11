ab0743c70ddd53789caafb87e40739b6aa8289f4
Addison Hall <ah@addisonhalldesign.com>
2025-07-11 16:06:19 -0500

    styles.php overhaul with new "like" class creation

--- inc/styles.php
+++ inc/styles.php
@@ -5,77 +5,138 @@
  *
  * These additional styles pull styling
  * info from customizations in GeneratePress.
- * It requires the Colors portion of the
- * premium add-on.
  *
  * @package GenerateChild
- * @link https://generatepress.com/downloads/generate-colors/
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Generate utility styles based on values in customizer
+ */
 if ( ! function_exists( 'gpc_base_inline_css_func' ) ) :
     
     function gpc_base_inline_css_func() {
-        
-        // Get our settings
-        $gpc_settings = wp_parse_args( 
+
+        if ( ! Utils_Themes::is_active_or_parent( 'GeneratePress' ) ) return;
+
+        // get GeneratePress settings
+        $font_settings = wp_parse_args(
             get_option( 'generate_settings', array() ),
-            generate_get_color_defaults()
+            generate_get_default_fonts()
         );
-        
+
+        $typography_arr = $font_settings['typography'];
+
         // Initiate our CSS class
         $gpc_css = new GeneratePress_CSS;
-        
-        // Grab our theme colors from inc/colors.php
-        global $gpc_theme_colors;
-        
-        // Add theme color custom properties
-        $gpc_css->set_selector( ':root' );
-        for( $i = 0; $i < count( $gpc_theme_colors ); $i++ ) {
-            $gpc_css->add_property( '--' . esc_attr( $gpc_theme_colors[$i]['slug'] ), esc_attr( $gpc_theme_colors[$i]['color'] ) );
-        }
-        
-        // Add standard WP classes based on our theme colors
-        for( $i = 0; $i < count( $gpc_theme_colors ); $i++ ) {
-            $color_slug = esc_attr( $gpc_theme_colors[$i]['slug'] );
-            $color_hex = esc_attr( $gpc_theme_colors[$i]['color'] );
-            $gpc_css->set_selector( '.has-' . $color_slug . '-color, body .editor-styles-wrapper .has-' . $color_slug . '-color, .wp-block-button__link.has-text-color.has-' . $color_slug . '-color' );
-            $gpc_css->add_property( 'color', $color_hex );
-            $gpc_css->set_selector( '.has-' . $color_slug . '-background-color, body .editor-styles-wrapper .has-' . $color_slug . '-background-color' );
-            $gpc_css->add_property( 'background-color', $color_hex );
-        }
-        
-        // Form button-outline
-        $gpc_css->set_selector( '.button.button-outline, .button.button-outline:visited, .woocommerce .button.button-outline, .woocommerce .button.button-outline:visited' );
-        $gpc_css->add_property( 'border-color', esc_attr( $gpc_settings[ 'form_button_background_color' ] ) );
-        $gpc_css->add_property( 'color', esc_attr( $gpc_settings[ 'form_button_background_color' ] ) );
-        
-        // Form button-outline hover
-        $gpc_css->set_selector( '.button.button-outline:hover, .button.button-outline:focus, .woocommerce .button.button-outline:hover, .woocommerce .button.button-outline:focus' );
-        $gpc_css->add_property( 'border-color', esc_attr( $gpc_settings[ 'form_button_background_color_hover' ] ) );
-        $gpc_css->add_property( 'color', esc_attr( $gpc_settings[ 'form_button_background_color_hover' ] ) );
 
-        // Allow us to hook CSS into our output
-        // do_action( 'gpc_base_inline_css', $gpc_css );
-        
+        $copy_elements = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+        $block_style_els_arr = [];
+
+        foreach( $typography_arr as $typography ) {
+            $element = $typography['selector'];
+            if ( in_array( $element, $copy_elements ) ) {
+
+                array_push( $block_style_els_arr, $element );
+
+                $gpc_css->set_selector( '.is-style-like-' . $element );
+                $gpc_css->add_property( 'font-family', $typography['fontFamily']);
+                $gpc_css->add_property( 'font-size', $typography['fontSize']);
+                $gpc_css->add_property( 'font-style', $typography['fontStyle']);
+                $gpc_css->add_property( 'font-weight', $typography['fontWeight']);
+                $gpc_css->add_property( 'letter-spacing', $typography['letterSpacing']);
+                $gpc_css->add_property( 'line-height', $typography['lineHeight']);
+                $gpc_css->add_property( 'margin-bottom', $typography['marginBottom']);
+                $gpc_css->add_property( 'text-transform', $typography['textTransform']);
+                
+                $gpc_css->start_media_query( generate_get_media_query( 'tablet' ) );
+                
+                $gpc_css->set_selector( '.is-style-like-' . $element );
+                $gpc_css->add_property( 'font-size', $typography['fontSizeTablet']);
+                $gpc_css->add_property( 'font-style', $typography['fontStyleTablet']);
+                $gpc_css->add_property( 'font-weight', $typography['fontWeightTablet']);
+                $gpc_css->add_property( 'letter-spacing', $typography['letterSpacingTablet']);
+                $gpc_css->add_property( 'line-height', $typography['lineHeightTablet']);
+                $gpc_css->add_property( 'margin-bottom', $typography['marginBottomTablet']);
+                $gpc_css->add_property( 'text-transform', $typography['textTransformTablet']);
+                
+                $gpc_css->stop_media_query();
+                
+                $gpc_css->start_media_query( generate_get_media_query( 'mobile' ) );
+                
+                $gpc_css->set_selector( '.is-style-like-' . $element );
+                $gpc_css->add_property( 'font-size', $typography['fontSizeMobile']);
+                $gpc_css->add_property( 'font-style', $typography['fontStyleMobile']);
+                $gpc_css->add_property( 'font-weight', $typography['fontWeightMobile']);
+                $gpc_css->add_property( 'letter-spacing', $typography['letterSpacingMobile']);
+                $gpc_css->add_property( 'line-height', $typography['lineHeightMobile']);
+                $gpc_css->add_property( 'margin-bottom', $typography['marginBottomMobile']);
+                $gpc_css->add_property( 'text-transform', $typography['textTransformMobile']);
+
+                $gpc_css->stop_media_query();
+            }
+        }
+
         // Return our dynamic CSS
-        return $gpc_css->css_output();
-        
+        return [
+            'css' => $gpc_css->css_output(),
+            'block_els' => $block_style_els_arr,
+        ];
+
     }
-    
+
 endif;
 
-add_action( 'wp_enqueue_scripts', 'gpc_add_inline_styles' );
-function gpc_add_inline_styles() {
-    $gpc_css = gpc_base_inline_css_func();
-    // this must reference an already queued stylesheet
-    wp_add_inline_style( 'gpc-base', $gpc_css );
+/**
+ * Create option to contain utility styles after saving customizer
+ */
+add_action( 'customize_save_after', 'gpc_save_gp_utility_css' );
+function gpc_save_gp_utility_css() {
+    if ( ! Utils_Themes::is_active_or_parent( 'GeneratePress' ) ) return;
+    $gpc_css_and_elements = gpc_base_inline_css_func();
+    update_option( 'gpc_utility_css', $gpc_css_and_elements['css'] );
+    update_option( 'gpc_utility_elements', $gpc_css_and_elements['block_els'] );
 }
 
-add_action( 'admin_enqueue_scripts', 'gpc_add_inline_admin_styles' );
-function gpc_add_inline_admin_styles() {
-    $gpc_admin_css = gpc_base_inline_css_func();
-    // this must reference an already queued stylesheet
-    wp_add_inline_style( 'gpc-editor', $gpc_admin_css );
+/**
+ * Check for utility css options and create block styles
+ */
+if ( function_exists( 'register_block_style' ) && get_option( 'gpc_utility_css' ) && get_option( 'gpc_utility_elements' ) ) {
+    
+    if ( ! Utils_Themes::is_active_or_parent( 'GeneratePress' ) ) return;
+
+    $elements = get_option( 'gpc_utility_elements' );
+
+    if ( ! $elements ) return;
+
+    foreach( $elements as $element ) {
+        register_block_style(
+            'core/heading',
+            array(
+                'name'         => 'like-' . $element,
+                'label'        => strtoupper( $element ) . ' Style'
+            )
+        );
+        register_block_style(
+            'core/paragraph',
+            array(
+                'name'         => 'like-' . $element,
+                'label'        => strtoupper( $element ) . ' Style'
+            )
+        );
+    }
+}
+
+/**
+ * Enqueue utility styles
+ */
+add_action( 'enqueue_block_assets', 'gpc_add_inline_styles' );
+function gpc_add_inline_styles() {
+    if ( ! Utils_Themes::is_active_or_parent( 'GeneratePress' ) ) return;
+    $gpc_css = get_option( 'gpc_utility_css' );
+    if ( ! $gpc_css ) return;
+    wp_register_style( 'gpc-utility', false );
+    wp_enqueue_style( 'gpc-utility' );
+    wp_add_inline_style( 'gpc-utility', $gpc_css );
 }
