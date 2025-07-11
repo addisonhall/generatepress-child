4eef8f123b9146eea682bff191ec8966e62800a3
Addison Hall <ah@addisonhalldesign.com>
2025-07-11 16:05:06 -0500

    New theme and plugin utilities in helper-functions

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -8,6 +8,141 @@
  */
 
 /**
+ * Class to check for theme stuff
+ */
+class Utils_Themes {
+
+    /**
+     * Check if specified theme is currently active
+     * 
+     * @param string $theme_name Theme name as specified in theme style.css
+     * @return bool True if theme is currently active, false otherwise.
+     */
+    public static function is_active( $theme_name ) {
+        $current_theme = wp_get_theme();
+        $is_theme_active = false;
+        if ( $current_theme === $theme_name ) {
+            $is_theme_active = true;
+        }
+        return $is_theme_active;
+    }
+
+    /**
+     * Check if specified theme is currently active parent theme
+     * 
+     * @param string $theme_name Theme name as specified in theme style.css
+     * @return bool True if theme is currently active parent theme, false otherwise.
+     */
+    public static function is_parent( $theme_name ) {
+        $current_theme = wp_get_theme();
+        $is_theme_active = false;
+        if ( $current_theme->parent()->get( 'Name' ) === $theme_name ) {
+            $is_theme_active = true;
+        }
+        return $is_theme_active;
+    }
+
+    /**
+     * Check if specified theme is currently active or parent theme
+     * 
+     * @param string $theme_name Theme name as specified in theme style.css
+     * @return bool True if theme is currently active or parent, false otherwise.
+     */
+    public static function is_active_or_parent( $theme_name ) {
+        $current_theme = wp_get_theme();
+        $is_theme_active = false;
+        if ( $current_theme === $theme_name || $current_theme->parent()->get( 'Name' ) === $theme_name ) {
+            $is_theme_active = true;
+        }
+        return $is_theme_active;
+    }
+}
+
+/**
+ * Class to check if plugin is active
+ * 
+ * @link https://github.com/mymizan/wp-check-if-plugin-active
+*/
+class Utils_Plugins {
+
+	/**
+	 * Check if a plugin is active
+	 *
+	 * @param string $plugin_main_file main file of the plugin, eg. woocommerce.php
+	 * @return bool True if plugin is active, false otherwise.
+	 */
+	public static function is_active( $plugin_main_file ) {
+		// get the list of plugins.
+		$active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+
+		// escape characters that have special meaning in regex.
+		$plugin_main_file    = preg_quote( $plugin_main_file, '/' );
+		$is_plugin_installed = false;
+
+		// Loop through the active plugins.
+		foreach ( $active_plugins as $plugin ) {
+			if ( preg_match( '/.+\/' . $plugin_main_file . '/', $plugin ) ) {
+				$is_plugin_installed = true;
+				break;
+			}
+		}
+
+		return $is_plugin_installed;
+	}
+
+	/**
+	 * Check if a plugin is network active
+	 *
+	 * @param string $plugin_main_file main file of the plugin, eg. woocommerce.php
+	 * @return bool True if plugin is active, false otherwise.
+	 */
+	public static function is_network_active( $plugin_main_file ) {
+
+		// if not a multisite, don't check.
+		if ( ! is_multisite() ) {
+			return false;
+		}
+
+		// get the list of plugins.
+		$active_plugins = get_site_option( 'active_sitewide_plugins' );
+
+		// escape characters that have special meaning in regex.
+		$plugin_main_file = preg_quote( $plugin_main_file, '/' );
+		$is_plugin_active = false;
+
+		// Loop through the active plugins.
+		foreach ( $active_plugins as $plugin_name => $plugin_activation ) {
+			if ( preg_match( '/.+\/' . $plugin_main_file . '/', $plugin_name ) ) {
+				$is_plugin_active = true;
+				break;
+			}
+		}
+
+		return $is_plugin_active;
+	}
+
+	/**
+	 * Check if a must use (mu) plugin exists.
+	 *
+	 * mu plugins are always active. So there's no need to check if they are
+	 * active or not. We just need to check that they are in the list.
+	 *
+	 * @param string $plugin_main_file main file of the plugin, eg. woocommerce.php
+	 * @return bool True if plugin matches, false otherwise.
+	 */
+	public static function is_mu_active( $plugin_main_file ) {
+		$_mu_plugins = get_mu_plugins();
+
+		if ( isset( $_mu_plugins[ $plugin_main_file ] ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
+
+/**
  * Return animation delay times.
  * 
  * @param int $pos Integer to select array position.
